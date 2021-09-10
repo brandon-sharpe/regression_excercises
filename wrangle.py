cbe234220fbf997d1ad4ce8f909d156cf6470813
brandon-sharpe <brandon.robert.sharpe@gmail.com>
2021-09-10 00:33:10 -0500

    addedexcercises i forgot to push yesterday

--- wrangle.py
+++ wrangle.py
@@ -1,3 +1,5 @@
+import warnings
+warnings.filterwarnings("ignore")
 import pandas as pd
 import numpy as np
 import os
@@ -5,6 +7,7 @@ from env import user, host, password
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
+import sklearn.preprocessing
 
 #**************************************************Acquire*******************************************************
 
@@ -111,7 +114,7 @@ def get_box(df):
     ''' Gets boxplots of acquired continuous variables'''
     
     # List of columns
-    cols = ['bedrooms', 'bathrooms', 'sqft', 'taxvalue', 'taxamount']
+    cols = ['bedrooms', 'bathrooms', 'sqft', 'taxvalue']
 
     plt.figure(figsize=(16, 3))
 
@@ -137,6 +140,37 @@ def get_box(df):
 
     plt.show()
 
+   #**************************************************Scale*******************************************************
+
+def add_scaled_columns(train, validate, test, scaler, columns_to_scale):
+    
+    # new column names
+    new_column_names = [c + '_scaled' for c in columns_to_scale]
+    
+    # Fit the scaler on the train
+    scaler.fit(train[columns_to_scale])
+    
+    # transform train validate and test
+    train = pd.concat([
+        train,
+        pd.DataFrame(scaler.transform(train[columns_to_scale]), columns=new_column_names, index=train.index),
+    ], axis=1)
+    
+    validate = pd.concat([
+        validate,
+        pd.DataFrame(scaler.transform(validate[columns_to_scale]), columns=new_column_names, index=validate.index),
+    ], axis=1)
+    
+    
+    test = pd.concat([
+        test,
+        pd.DataFrame(scaler.transform(test[columns_to_scale]), columns=new_column_names, index=test.index),
+    ], axis=1)
+    
+    return train, validate, test
+
+ 
+    
 #**************************************************Prepare*******************************************************
 
 def prepare_zillow():
@@ -148,6 +182,9 @@ def prepare_zillow():
     
     # Drop the null in every column(less than .006% of the data)
     df = df.dropna()
+    
+    # drop tax taxamount due to not actually having this knowledge until the following year. Data leak
+    df = df.drop(columns=['taxamount'])
     
     # Remove the outliers
     k=1.5
@@ -172,10 +209,13 @@ def prepare_zillow():
     get_hist(df)
     get_box(df)
     
+  
+    
     # train/validate/test split
-    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
-    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    train_validate, test = train_test_split(df, test_size=.2, random_state=617)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=617)
     return train, validate, test 
+
 
 
 #**************************************************Wrangle*******************************************************
@@ -184,5 +224,73 @@ def prepare_zillow():
 def wrangle_zillow():
     '''Acquire and prepare data from Zillow database for explore'''
     train, validate, test = prepare_zillow()
+    train, validate, test = add_scaled_columns(train,
+                                                   validate,
+                                                   test,
+                                                   scaler=sklearn.preprocessing.MinMaxScaler(),
+                                                   columns_to_scale=['bedrooms', 'bathrooms', 'sqft'])
     
+    return train, validate, test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#******************************Telco wrangle********************************************
+
+def new_telco_charge_data():
+    '''
+    Returns telco_charge into a dataframe
+    '''
+    sql_query = '''select customer_id, monthly_charges, tenure, total_charges from customers
+    join internet_service_types using(internet_service_type_id)
+    join contract_types using(contract_type_id)
+    join payment_types using(payment_type_id)
+    where contract_type_id = 3'''
+    df = pd.read_sql(sql_query, get_connection('telco_churn'))
+    return df 
+
+def get_telco_charge_data():
+    '''get connection, returns telco_charge into a dataframe and creates a csv for us'''
+    if os.path.isfile('telco_charge.csv'):
+        df = pd.read_csv('telco_charge.csv', index_col=0)
+    else:
+        df = new_telco_charge_data()
+        df.to_csv('telco_charge.csv')
+    return df
+
+def clean_telco(df):
+    '''cleans our data'''
+    df = df.replace(r'^\s*$', np.NaN, regex=True)
+    df.total_charges = pd.to_numeric(df.total_charges, errors='coerce').astype('float64')
+    df.total_charges = df.total_charges.fillna(value=df.total_charges.mean()).astype('float64')
+    df = df.set_index("customer_id")
+    return df 
+
+
+def split_telco(df):
+    '''
+    Takes in a cleaned df of telco data and splits the data appropriatly into train, validate, and test.
+    '''
+    
+    train_val, test = train_test_split(df, train_size =  0.8, random_state = 123)
+    train, validate = train_test_split(train_val, train_size =  0.7, random_state = 123)
+    return train, validate, test
+
+def wrangle_telco():
+    '''acquire and our dataframe, returns a df'''
+    df = clean_telco(get_telco_charge_data())
+    train, validate, test = split_telco(df)
     return train, validate, test
